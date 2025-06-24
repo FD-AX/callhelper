88e8ff88e758c9ffc358047b6e410517861767a4
fd-ax <lmaoleoayyy@gmail.com>
2025-06-24 14:59:34 +0300

    CORS

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI, WebSocket, Request, Response, HTTPException, status, Form, Depends
 from fastapi.responses import RedirectResponse, HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.model import graph
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/chat")
 def send_message( query: str):
